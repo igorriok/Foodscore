e2cec69b6b00a8b69b7749f8a95d81a324c8395f
igor <i.solonari@gmail.com>
2017-08-20 13:49:24 +0300

    First commit

---
+++ app/src/main/java/com/solonari/igor/foodscore/Maggy.java
@@ -0,0 +1,13 @@
+package com.solonari.igor.foodscore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Maggy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maggy);
+    }
+}
